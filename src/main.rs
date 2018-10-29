e62b4a6e328d09c27cf8a8e9b28595a203a6674b
Roman Frołow <rofrol@gmail.com>
2018-10-29 02:13:56 +0100

    Use to_str instead of  to_string_lossy

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 use std::fs;
 use std::env;
 use std::error::Error;
-
+use std::process;
 
 fn main() -> Result<(), Box<Error>> {
     let args: Vec<String> = env::args().collect();
@@ -18,8 +18,15 @@ fn main() -> Result<(), Box<Error>> {
     let canon_parent = Path::parent(&canon).unwrap();
     let file_stem  = input.file_stem().unwrap();
     let extension = input.extension().unwrap();
-    let output_name = file_stem.to_string_lossy() + ".pretty." + extension.to_string_lossy();
-    let output = canon_parent.join(output_name.as_ref());
-    fs::write(&output, data).expect("Unable to write file");
+    let output_option = file_stem.to_str()
+        .map(|x:&str| x.to_owned() + ".pretty.")
+        .and_then(|x:String| extension.to_str().and_then(|y:&str| Some(x + y)))
+        .map(|x:String| canon_parent.join(x));
+    if let Some(output) = output_option {
+        fs::write(&output, data).expect("Unable to write file");
+    } else {
+        eprintln!("There was problem with generating output name. Exiting.");
+        process::exit(1);
+    }
     Ok(())
 }
