b5c083f91ad703bef2776827e71a57ee6ce293c0
Roman Frołow <rofrol@gmail.com>
2018-10-29 03:47:53 +0100

    insert_segment_before_extension

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,36 @@ use std::fs;
 use std::env;
 use std::error::Error;
 use std::process;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<Error>> {
     let args: Vec<String> = env::args().collect();
     let input = Path::new(&args[1]);
-    let json_file = File::open(input).expect("file not found");
-    let value: serde_json::Value = serde_json::from_reader(json_file).expect("error while reading json");
-    let data = serde_json::to_string_pretty(&value).expect("Unable to pretty format json");
+    let json_file = File::open(input)?;
+    let value: serde_json::Value = serde_json::from_reader(json_file)?;
+    let data = serde_json::to_string_pretty(&value)?;
+    let output_option = insert_segment_before_extension(".pretty.", &input);
+    if let Ok(output) = output_option {
+        fs::write(&output, data)?;
+    } else {
+        eprintln!("There was problem with generating output name");
+        process::exit(1);
+    }
+    Ok(())
+}
+
+fn insert_segment_before_extension(segment: &str, input: &Path) -> Result<PathBuf, Box<Error>> {
     let canon = fs::canonicalize(&input)?;
     let canon_parent = Path::parent(&canon).expect("Cannot get parent");
     let file_stem  = input.file_stem().expect("Cannot get file_stem");
     let extension = input.extension().expect("Cannot get extension");
     let output_option = file_stem.to_str()
-        .map(|x:&str| x.to_owned() + ".pretty.")
+        .map(|x:&str| x.to_owned() + segment)
         .and_then(|x:String| extension.to_str().and_then(|y:&str| Some(x + y)))
         .map(|x:String| canon_parent.join(x));
     if let Some(output) = output_option {
-        fs::write(&output, data).expect("Unable to write file");
+        Ok(output)
     } else {
-        eprintln!("There was problem with generating output name. Exiting.");
-        process::exit(1);
+        Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "There was problem with generating output name")))
     }
-    Ok(())
 }
