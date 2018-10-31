5da5b93a790d39a71affeb9697df635c435ed77a
Roman Frołow <rofrol@gmail.com>
2018-10-31 13:58:25 +0100

    Convert Option to Result

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,22 @@ fn insert_segment_before_extension(segment: &str, input: &Path) -> Result<PathBu
     let canon_parent = Path::parent(&canon).expect("Cannot get parent");
     let file_stem = input.file_stem().expect("Cannot get file_stem");
     let extension = input.extension().expect("Cannot get extension");
-    let output_option = file_stem
+    // expected trait std::error::Error, found struct `std::io::Error`
+    // Ok(Command...?) converts between errors
+    // https://stackoverflow.com/questions/48430836/rust-proper-error-handling-auto-convert-from-one-error-type-to-another-with-que/48431339#48431339
+    // https://github.com/rust-lang-nursery/error-chain/issues/119#issuecomment-274957996
+    Ok(file_stem
         .to_str()
-        .map(|x: &str| x.to_owned() + segment)
-        .and_then(|x: String| extension.to_str().map(|y: &str| x + y))
-        .map(|x: String| canon_parent.join(x));
-    match output_option {
-        Some(output) => Ok(output),
-        None => Err(Box::new(std::io::Error::new(
+        .ok_or(Box::new(std::io::Error::new(
             std::io::ErrorKind::Other,
-            "There was a problem with generating output name",
-        ))),
-    }
+            "Failed on file_stem.to_str()",
+        ))).map(|x: &str| x.to_owned() + segment)
+        .and_then(|x: String| {
+            extension
+                .to_str()
+                .ok_or(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Failed on extension.to_str()",
+                ))).map(|y: &str| x + y)
+        }).map(|x: String| canon_parent.join(x))?)
 }
