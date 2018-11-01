46218b59511a1cde0616363a6e650ab07c4e911a
Roman Frołow <rofrol@gmail.com>
2018-11-01 03:59:45 +0100

    use ok_or_else so that Error::new is not run when result is Some

--- src/main.rs
+++ src/main.rs
@@ -24,40 +24,53 @@ fn main() -> Result<(), Box<Error>> {
         .and_then(|output| Ok(fs::write(&output, data)?))
 }
 
-fn insert_segment_before_extension(segment: &str, input_absolute: &Path) -> Result<PathBuf, Box<Error>> {
+fn insert_segment_before_extension(
+    segment: &str,
+    input_absolute: &Path,
+) -> Result<PathBuf, Box<Error>> {
     // expected trait std::error::Error, found struct `std::io::Error`
     // Ok(Command...?) converts between errors
     // https://stackoverflow.com/questions/48430836/rust-proper-error-handling-auto-convert-from-one-error-type-to-another-with-que/48431339#48431339
     // https://github.com/rust-lang-nursery/error-chain/issues/119#issuecomment-274957996
     Ok(input_absolute
         .file_stem()
-        .ok_or(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Cannot get file_stem",
-        ))).and_then(|file_stem| {
-            file_stem.to_str().ok_or(Box::new(std::io::Error::new(
+        .ok_or_else(|| {
+            Box::new(std::io::Error::new(
                 std::io::ErrorKind::Other,
-                "Failed on file_stem.to_str()",
-            )))
+                "Cannot get file_stem",
+            ))
+        }).and_then(|file_stem| {
+            file_stem.to_str().ok_or_else(|| {
+                Box::new(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Failed on file_stem.to_str()",
+                ))
+            })
         }).map(|x: &str| x.to_owned() + segment)
         .and_then(|x: String| {
             // TODO: What if there is no extension?
             input_absolute
                 .extension()
-                .ok_or(Box::new(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    "Cannot get extension",
-                ))).and_then(|extension| {
-                    extension.to_str().ok_or(Box::new(std::io::Error::new(
+                .ok_or_else(|| {
+                    Box::new(std::io::Error::new(
                         std::io::ErrorKind::Other,
-                        "Failed on extension.to_str()",
-                    )))
+                        "Cannot get extension",
+                    ))
+                }).and_then(|extension| {
+                    extension.to_str().ok_or_else(|| {
+                        Box::new(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            "Failed on extension.to_str()",
+                        ))
+                    })
                 }).map(|y: &str| x + y)
         }).and_then(|x: String| {
             Path::parent(&input_absolute)
-                .ok_or(Box::new(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    "Cannot get parent",
-                ))).map(|parent| parent.join(x))
+                .ok_or_else(|| {
+                    Box::new(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Cannot get parent",
+                    ))
+                }).map(|parent| parent.join(x))
         })?)
 }
